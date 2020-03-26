443f00014094f092455d6ea3836fe8ee65d4e7de
Neyamat69 <neyamatullah100@gmail.com>
2020-03-26 19:54:51 +0600

    Done CRUD operations for the users, roles and categories

---
+++ InventoryManagementSystem/Controllers/RoleController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class RoleController : Controller
+    {
+        private InventoryDBEntities db = new InventoryDBEntities();
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View(db.roles.ToList());
+        }
+
+        // GET: Role/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Role/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "r_id,r_name,r_status")] role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Role/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "r_id,r_name,r_status")] role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            role role = db.roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            role role = db.roles.Find(id);
+            db.roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
